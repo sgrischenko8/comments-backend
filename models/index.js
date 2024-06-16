77fbe196fec155809c0305cff28206d483b39269
sgrischenko8 <sgrischenko8@gmail.com>
2024-06-16 22:48:59 +0300

    the database is replaced by MySQL

--- models/index.js
+++ models/index.js
@@ -1,9 +1,20 @@
 // models/index.js
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite", // Путь к файлу базы данных
+const sequelize = new Sequelize("mydatabase", "myuser", "mypassword", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306, // порт MySQL
+  define: {
+    timestamps: true, // опционально, если вам нужны временные метки
+    createdAt: "createdAt", // если нужно настраивать название поля для createdAt
+    updatedAt: false, // если не нужно использовать updatedAt
+  },
 });
+
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./database.sqlite", // Путь к файлу базы данных
+// });
 
 module.exports = sequelize;
