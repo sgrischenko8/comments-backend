1eea72d594ff8b1e99a7828363d6005a8701acde
sgrischenko8 <sgrischenko8@gmail.com>
2024-06-21 01:49:38 +0300

    Update commentController.js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -67,10 +67,10 @@ async function addComment(req, res) {
       sortOrder = "DESC",
       page = 1,
     } = req.body;
+    console.log(req.cookies, "=======crypted captcha");
     if (!userName || !email || !text || !cryptedCaptcha) {
       return res.status(400).json({ error: "All fields are required" });
     }
-    console.log(req.cookies, "=======crypted captcha");
     console.log(captcha, "======= captcha");
     if (extractCaptcha(cryptedCaptcha) !== captcha) {
       return res.status(400).json({ error: "Invalid CAPTCHA" });
