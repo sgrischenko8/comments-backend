5e01f1a3135e3797276a4782022f553320ccda1e
sgrischenko8 <sgrischenko8@gmail.com>
2024-06-21 01:39:47 +0300

    Update jwtService.js

--- utils/jwtService.js
+++ utils/jwtService.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-
+console.log(jwt, "jwt========");
 exports.signCaptcha = (captcha) =>
   jwt.sign({ captcha }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -7,6 +7,7 @@ exports.signCaptcha = (captcha) =>
 exports.extractCaptcha = (token) => {
   try {
     const { captcha } = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(jwt.verify(token, process.env.JWT_SECRET), "jwt ver========");
     return captcha;
   } catch (error) {
     console.log(error.message);
